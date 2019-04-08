1544b083b7ec18f26378670d7b19da32eee4473f
alruiz_o <alruiz_o@CCTL131-25.unirioja.loc>
2019-04-08 13:29:48 +0200

    haciendo pedidos.jsp

---
+++ src/java/sol/ser/PedidosCliente.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sol.ser;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import paw.bd.GestorBD;
+import paw.bd.GestorBDPedidos;
+import paw.model.Cliente;
+import paw.model.ExcepcionDeAplicacion;
+import paw.model.PedidoEnRealizacion;
+
+/**
+ *
+ * @author alruiz_o
+ */
+public class PedidosCliente extends HttpServlet {
+    private GestorBDPedidos gbdP = new GestorBDPedidos();
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            HttpSession sesion = request.getSession();
+            Cliente cliente = (Cliente) sesion.getAttribute("cliente");
+            String codigoCliente = cliente.getCodigo();
+            if((codigoCliente == null) || (codigoCliente.equals(""))){
+                
+            }
+            request.setAttribute("pedidosPendientes", gbdP.getPedidosPendientes(codigoCliente));
+            request.setAttribute("pedidosCompletados", gbdP.getPedidosCompletados(codigoCliente));
+            request.setAttribute("pedidosAnulados", gbdP.getPedidoAnulado(codigoCliente));
+            RequestDispatcher rd = request.getRequestDispatcher("../clientes/pedidosCliente.jsp");
+            rd.forward(request, response);
+        } catch (ExcepcionDeAplicacion ex) {
+            Logger.getLogger(PedidosCliente.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
--- web/clientes/modificarDatosCliente.jsp
+++ web/clientes/modificarDatosCliente.jsp
@@ -5,7 +5,8 @@
 --%>
 
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
     <head>
